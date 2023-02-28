a7c71609085ccff41b15a432cd1c5476ef20f923
ayman mansour <am5432@fayoum.edu.eg>
2023-02-28 22:23:32 +0200

    User

---
+++ User/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from django.utils.translation import ugettext_lazy
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100,unique=True)
+    email = models.EmailField(('email address'), unique=True)
+   
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    spouse_name = models.CharField(blank=True, max_length=100)
+    date_of_birth = models.DateField(blank=True, null=True)
+    
+
+    def __str__(self):
+        return self.email
+
+
