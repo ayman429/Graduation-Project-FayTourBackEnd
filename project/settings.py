81a105e21ef494d7812e25d98fa574164e3554bd
ayman mansour <am5432@fayoum.edu.eg>
2023-03-10 15:40:36 +0200

    Hotels_TouristPlaces

--- project/settings.py
+++ project/settings.py
@@ -53,15 +53,15 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
     
 
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ),
   
     'DEFAULT_RENDERER_CLASSES': [
             'rest_framework.renderers.JSONRenderer',
