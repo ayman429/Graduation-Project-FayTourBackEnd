37e3eb4d226affdddba7bc32d83c950e9b2e1100
ayman mansour El_siad <73555334+ayman429@users.noreply.github.com>
2023-03-10 17:01:04 +0200

    Update urls.py

--- User/urls.py
+++ User/urls.py
@@ -6,8 +6,8 @@ from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-    url(r'^account/', include('allauth.urls')),
-    url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('account/', include('allauth.urls')),
+    path('accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
 ]
